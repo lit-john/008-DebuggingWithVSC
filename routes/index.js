5540ab909751835c79403b368d13849a8498c1c8
unknown <john.hannafin@A124-LSAD-JH.litdom.lit.ie>
2016-02-29 11:37:47 +0000

    Add a process.env.TITLE line

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var myTitle = process.env.TITLE;
+    console.log(process.env);
+    res.render('index', { title: myTitle });
 });
 
 module.exports = router;
