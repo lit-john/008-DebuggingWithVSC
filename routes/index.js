f9fab70d75b1952d5b7816a62bc1f40b1c9f44d7
unknown <john.hannafin@A124-LSAD-JH.litdom.lit.ie>
2016-02-29 11:38:43 +0000

    Incase process.env.TITLE is not defined

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var myTitle = process.env.TITLE;
+    var myTitle = process.env.TITLE || 'Express';
     console.log(process.env);
     res.render('index', { title: myTitle });
 });
